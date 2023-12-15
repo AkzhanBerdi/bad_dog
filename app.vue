959571191d19215ee35c6bb927cd78dd2147f71a
AkzhanBerdi <akzhan.berdeyev@gmail.com>
2023-12-16 02:42:42 +0600

    first draft 9.7

--- app.vue
+++ app.vue
@@ -6,8 +6,8 @@
           <h1 class="text-2xl font-semibold">
             <NuxtLink to="/">Bad Dog Data</NuxtLink>
           </h1>
-          <p class="text-gray-500">From: Basic Statistics</p>
-          <p class="text-gray-500">To: Machine Learning Algorithms</p>
+          <p class="text-gray-500">From: Basic Stats</p>
+          <p class="text-gray-500">To: Machine Learning</p>
         </div>
 
         <div class="align-right">
