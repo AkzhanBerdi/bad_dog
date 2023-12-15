4950d03c0fdb7e00ea90779aab64fe3586c02baf
AkzhanBerdi <akzhan.berdeyev@gmail.com>
2023-12-16 02:58:58 +0600

    first draft 10.3

--- app.vue
+++ app.vue
@@ -6,7 +6,7 @@
           <h1 class="text-2xl font-semibold">
             <NuxtLink to="/">Bad Dog Data</NuxtLink>
           </h1>
-          <p class="text-gray-500">Self-Taught Data Sceintist</p>
+          <p class="text-gray-500">Self-Taught Data Scientist Journal</p>
         </div>
 
         <div class="align-right">
