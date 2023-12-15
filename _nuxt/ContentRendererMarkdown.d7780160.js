09190a988dbd996772c933f9bd36919bd4d51dfb
AkzhanBerdi <akzhan.berdeyev@gmail.com>
2023-12-15 19:48:47 +0600

    Add public files

---
+++ _nuxt/ContentRendererMarkdown.d7780160.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./ContentRendererMarkdown.vue.c7abf039.js";import"./entry.ee826f3e.js";import"./index.b0fe9fac.js";import"./preview.60869737.js";export{o as default};
---
+++ _nuxt/ProseCode.94eb65dd.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./ProseCode.vue.d80d475b.js";import"./entry.ee826f3e.js";export{m as default};
