dcff5e76f8fa2fe6d32887ce4134daf2545f7125
AkzhanBerdi <akzhan.berdeyev@gmail.com>
2023-12-29 16:33:10 +0600

    npm install google-analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,18 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     //"nuxt-content-assets",
+    "@nuxtjs/google-analytics",
     "@nuxtjs/tailwindcss", 
     "@nuxt/content"
   ],
   content: {
+  },
+  googleAnalytics: {
+    id: 'G-VSDTCMMC1P'
+  },
+  publicRuntimeConfig: {
+    googleAnalytics: {
+      id: process.env.GOOGLE_ANALYTICS_ID
+    }
   },
   });
