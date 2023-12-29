9d216a8709330e8ebc735e755b2dace91f8ac3b2
AkzhanBerdi <akzhan.berdeyev@gmail.com>
2023-12-29 16:11:50 +0600

    google tag manager added

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YF25TVCCJC"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YF25TVCCJC');
+</script>
 <template>
   <div class="max-w-3xl px-4 mx-auto">
     <!-- <Header> -->
