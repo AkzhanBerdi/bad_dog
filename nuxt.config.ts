ae2d6e07c3141b5e44abec9c5e5ae82f33c96152
akzhan.berdeyev <akzhan.berdeyev@gmail.com>
2025-12-13 15:04:29 +0500

    changing-favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,21 +38,22 @@ export default defineNuxtConfig({
     }
   },
 
-  app: {
-    head: {
-      viewport: 'width=device-width, initial-scale=1',
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css',
-        },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icon-152x152.png' },
-        { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon-120x120.png' },
-        { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-touch-icon-76x76.png' }
-      ]
-    }
-  },
+app: {
+  head: {
+    viewport: 'width=device-width, initial-scale=1',
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }, // Add this line
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.16.9/dist/katex.min.css',
+      },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-touch-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-touch-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-touch-icon-76x76.png' }
+    ]
+  }
+},
 
   compatibilityDate: '2024-10-27'
 })
